16c22c82c6a123d22f1a5f62c1756745c3ab76cf
Render3 <studio@motionhive.tv>
2025-02-14 00:31:20 +0200

    refactored slightly

---
+++ src/server/functions/process/AERenderVersion/jobs/edit.ts
@@ -0,0 +1,119 @@
+import fs from 'fs';
+import { MYSQL_DB } from "../../../../../classes/MYSQL_DB";
+import { AE } from "../../../../../types/AE";
+import { CORE } from "../../../../../types/CORE";
+import { DB } from "../../../../../types/DB";
+import { TimeDeltas } from "../../../../../V2/classes/TimeDeltas";
+import { appendToLogFile } from "../../../../../V2/utils/appendToLog";
+import updateJob from "../../../db/updateJob";
+import handleGoogleDriveReadError__AERENDER from "../../../error/handleGoogleDriveRead__AERENDER";
+import recognizeError from "../../../error/recognize";
+import getBrand from "../../../get/brand";
+import getEdition from "../../../get/edition";
+import getProduct from "../../../get/product";
+import getTimestamp from "../../../get/timestamp";
+import { GenericProcessProps, EDIT } from "../../EDIT";
+import { VictorResult } from "../processVictorResult";
+
+// will be used to check if system is busy
+const systemBusyFilePath = `G:/My Drive/Sports/systemBusy.txt`;
+
+export async function editSingleFreshJob(
+    RM: DB.RenderMachine,
+    TD: TimeDeltas,
+    job: AE.Job,
+    SportsDB: MYSQL_DB,
+    BackofficeDB: MYSQL_DB,
+    logFileName: string,
+    debugMode: boolean = false
+): Promise<void> {
+    const funcName = `editSingleFreshJob`;
+
+    let nextMessage = '';
+
+    try {
+        // write systemBusy file
+        !debugMode && fs.writeFileSync(systemBusyFilePath, 'true');
+
+        try {
+            nextMessage = `${funcName}: Next job: ${job.brand_name} ${job.product_name} ${job.lang}`;
+            appendToLogFile(TD, nextMessage, logFileName, true, 'cyan');
+
+            const edition: CORE.Edition = await getEdition(SportsDB, job, TD.editionDateYYYYMMDD);
+            const brand: CORE.Brand = await getBrand(SportsDB, job.brand_name);
+            const product: CORE.Product = await getProduct(
+                SportsDB,
+                job.product_name
+            );
+
+            // throw JSON.stringify(edition, null, 4);
+
+            let processProps: GenericProcessProps = {
+                TD,
+                SportsDB,
+                BackofficeDB,
+                brand,
+                edition,
+                product,
+                dbgLevel: 1,
+            };
+
+            if (!(product.product_name in EDIT))
+                throw `No process found for ${product.product_name}`;
+            let victorResult: VictorResult = await EDIT[product.product_name](
+                processProps
+            );
+
+            nextMessage = `victor result: ${JSON.stringify(victorResult, null, 4)}`;
+            appendToLogFile(TD, nextMessage, logFileName, true, 'magenta');
+
+            let potentialErrorName = recognizeError(victorResult.message || '');
+
+            // only in the event of a googleDriveRead error, we'll retry the process.
+            if (potentialErrorName === 'googleDriveRead')
+                victorResult = await handleGoogleDriveReadError__AERENDER({
+                    product,
+                    processProps,
+                    victorResult,
+                });
+
+            potentialErrorName = recognizeError(victorResult.message || '');
+
+            // if there's an error that is not of the following types, throw the result.
+            if (
+                !(
+                    potentialErrorName === 'success' ||
+                    potentialErrorName === 'empty' ||
+                    potentialErrorName === 'context'
+                )
+            ) {
+                // Let's try not updating the job status to error, so that we can retry the process.
+                // await updateJob({ SportsDB, nextJob, log, newStatus: 'error' });
+                // throw victorResult.message;
+            }
+
+            nextMessage = `Edit completed successfully (${potentialErrorName})`;
+            appendToLogFile(TD, nextMessage, logFileName, true, 'green');
+
+            await updateJob({
+                SportsDB,
+                nextJob: job,
+                log: '',
+                newStatus: 'edited',
+            });
+
+            // return victorResult;
+        } catch (e) {
+            // handle error
+            nextMessage = `${job.brand_name} ${job.lang} ${
+                job.product_name
+            } failed @ ${getTimestamp()} with error: ${e}`;
+            appendToLogFile(TD, nextMessage, logFileName, true, 'red');
+        }
+    } catch (e) {
+        throw `${funcName}: ${e}`;
+    } finally {
+        // write systemBusy file
+        !debugMode && fs.writeFileSync(systemBusyFilePath, 'false');
+    }
+}
---
+++ src/server/functions/process/AERenderVersion/jobs/render.ts
@@ -0,0 +1,146 @@
+import { MYSQL_DB } from "../../../../../classes/MYSQL_DB";
+import { coreTables } from "../../../../../constants/coreTables";
+import identifyRenderMachine from "../../../../../functions/identifyRenderMachine";
+import { PATHS } from "../../../../../functions/PATHS";
+import { getGeneralPaths } from "../../../../../functions/R2R/components/getGeneralPaths";
+import { getSubfolderStrucure } from "../../../../../functions/R2R/components/getSubfolderStructure";
+import { AE } from "../../../../../types/AE";
+import { CORE } from "../../../../../types/CORE";
+import { Paths } from "../../../../../types/CORE/Paths";
+import { DB } from "../../../../../types/DB";
+import { AERender } from "../../../../../V2/classes/AERender";
+import { TimeDeltas } from "../../../../../V2/classes/TimeDeltas";
+import { getAERenderPath } from "../../../../../V2/config/constants/getAERenderPath";
+import { appendToLogFile } from "../../../../../V2/utils/appendToLog";
+import updateJob from "../../../db/updateJob";
+import getBrand from "../../../get/brand";
+import getEdition from "../../../get/edition";
+import getProduct from "../../../get/product";
+
+export async function renderSingleEditedJob(
+    RM: DB.RenderMachine,
+    TD: TimeDeltas,
+    job: AE.Job,
+    SportsDB: MYSQL_DB,
+    BackofficeDB: MYSQL_DB,
+    logFileName: string,
+    debugMode: boolean = false
+) {
+    const funcName = `renderSingleEditedJob`;
+
+    try {
+        // write the initial log message into a new log file
+        appendToLogFile(TD, `${funcName}: Test started`, logFileName, true, 'pink');
+
+        const aeRenderPath = getAERenderPath();
+
+        const edition: CORE.Edition = await getEdition(SportsDB, job, TD.editionDateYYYYMMDD);
+        const brand: CORE.Brand = await getBrand(SportsDB, job.brand_name);
+        const product: CORE.Product = await getProduct(
+            SportsDB,
+            job.product_name
+        );
+        const renderMachine: DB.RenderMachine = await identifyRenderMachine(
+            SportsDB
+        );
+
+        const generalFolderPaths: Paths.GeneralFolders = await getGeneralPaths(
+            renderMachine,
+            SportsDB
+        );
+
+        /**
+         * get the buleprint of the subfolder structure for the given product.
+         */
+        let productSubfolders: CORE.AE.ProductSubFolder[] =
+            await SportsDB.SELECT(coreTables.product_subfolders, {
+                whereClause: { product_name: edition.product_name },
+            });
+
+        const subFolders = getSubfolderStrucure(
+            productSubfolders,
+            renderMachine,
+            edition,
+            brand,
+            product,
+            generalFolderPaths
+        );
+
+        const timeLimit = 5000;
+
+        const abortController = new AbortController();
+
+        /**
+         * Now we take @param paths.projectSaveFile and generate
+         * @param paths.exportFile
+         */
+        const paths: AE.Json.AbsolutePath.Obj = PATHS.getAll__CORE(
+            subFolders,
+            edition
+        );
+        
+        /**
+         * Let's get the file name and export folder path
+         * from @param paths.exportFile
+         */
+        const cleanedExportFilePath = paths.exportFile.replace(/\\/g, '/');
+        const splitExportFilePath = cleanedExportFilePath.split('/');
+        const exportFileName = splitExportFilePath.pop();
+        const exportFolderPath = splitExportFilePath.join('/');
+
+        if (!exportFileName) throw `No export file name found`;
+        if (!exportFolderPath) throw `No export folder path found`;
+
+        const renderCompName = `0_Main comp_AERender`;
+
+        const aeRender = new AERender(
+            TD,
+            logFileName,
+            aeRenderPath,
+            paths.projectSaveFile,
+            exportFolderPath,
+            exportFileName,
+            renderCompName,
+            timeLimit,
+            abortController
+        );
+
+        try {
+            await aeRender.execPromiseInstance;
+
+            const nextMessage = `Render completed successfully`;
+            appendToLogFile(TD, nextMessage, logFileName, true, 'green');
+
+            await updateJob({
+                SportsDB,
+                nextJob: job,
+                log: '',
+                newStatus: 'rendered',
+                prevStatus: 'edited',
+            });
+            
+        } catch (error) {
+            if ((error as Error).name === 'AbortError') {
+                console.warn(
+                    'Render process aborted. error.name === "AbortError"'
+                );
+                appendToLogFile(
+                    TD,
+                    'Render process aborted. error.name === "AbortError"',
+                    logFileName
+                );
+            } else {
+                console.error('Render process failed:', error);
+                appendToLogFile(
+                    TD,
+                    `Render process failed: ${(error as Error).message}`,
+                    logFileName
+                );
+            }
+        }
+    } catch (e) {
+        const errorMessage = `Caught Error: ${e}`;
+        console.warn(errorMessage);
+        appendToLogFile(TD, errorMessage, logFileName);
+    }
+}
