aeecdf7e88419f40cf3767cb857241d8f48c0e99
Render3 <studio@motionhive.tv>
2025-02-24 18:17:17 +0200

    Added some explicitness to index

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import * as schedule from "node-schedule";
 import SERVER_MAIN from "./server";
 import reorganizeTransNews from "./DB";
 
-// const customDate = new Date(2025, 1, 20, 1, 0, 0, 0);
+const customDate = new Date(2025, 1, 24, 1, 0, 0, 0);
+const logToConsole = true;
+const debugMode = true;
+const wastefulMode = true;
+SERVER_MAIN(logToConsole, debugMode, wastefulMode, customDate);
 
 // every three minutes
 // schedule.scheduleJob('*/10 * * * *', () => SERVER_MAIN(true));
@@ -10,5 +14,4 @@ import reorganizeTransNews from "./DB";
 // every day at midnight
 // schedule.scheduleJob('3 0 * * *', reorganizeTransNews);
 
-SERVER_MAIN(true, true);
 // reorganizeTransNews();
